fa54d5e1f9c4a1377716177c13d1625d8fce369e
Giancarlo Fusiello <GiancarloFusiello@users.noreply.github.com>
2021-04-30 13:01:25 +0100

    add set -e to exit script in case of an error

--- docker-start-script.sh
+++ docker-start-script.sh
@@ -1,7 +1,12 @@
+
+set -e
+
 # start xvfb
 /opt/bin/start-xvfb.sh &
 
 # start vnc server
 /opt/bin/start-vnc.sh > /dev/null 2>&1 &
 
+echo " "
 echo "You can now connect your VNC client to http://localhost:5900 to view the application GUI. Use the password 'secret' if prompted"
+echo " "
