f49f0f7e5345d97aaf6d517a50e39b1734da6077
Giancarlo Fusiello <GiancarloFusiello@users.noreply.github.com>
2021-04-30 09:13:07 +0100

    suppress x11vnc output at startup and run in background. update output to include vnc password.

--- docker-start-script.sh
+++ docker-start-script.sh
@@ -2,6 +2,6 @@
 /opt/bin/start-xvfb.sh &
 
 # start vnc server
-/opt/bin/start-vnc.sh &
+/opt/bin/start-vnc.sh > /dev/null 2>&1 &
 
-echo "You can now connect your VNC client to http://localhost:5900 to view the application GUI\n"
+echo "You can now connect your VNC client to http://localhost:5900 to view the application GUI. Use the password 'secret' if prompted"
